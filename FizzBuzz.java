fe2094f212c0df13508ef3a899613caedcf26a2b
Felix <felix-mayer@gmx.de>
2021-06-10 16:57:04 +0200

    first commit

---
+++ FizzBuzz.java
@@ -0,0 +1,14 @@
+public class FizzBuzz {
+	public String fizzBuzz(int number) {
+		if( number % 3 == 0 && number % 5 == 0) {
+			System.out.println("FizzBuzz");
+		} else if(number % 3 == 0) {
+			System.out.println("Fizz");
+		} else if(number % 5 == 0) {
+			System.out.println("Buzz");
+		} else {
+			System.out.println("Number not devisible by 3 or 5.");
+		}
+		return String.valueOf(number);
+	}
+}
